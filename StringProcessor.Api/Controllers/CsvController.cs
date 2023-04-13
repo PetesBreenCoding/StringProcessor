29301f427c9b8fbafd999ddbf8668949796cbcd9
Peter Breen <PBreen@UNUM.COM>
2023-04-13 11:19:17 +0100

    Starting point

---
+++ StringProcessor.Api/Controllers/CsvController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StringProcessor.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CsvController
+{
+	[HttpPost(Name = "lines-to-csv")]
+	public string LinesToCsv([FromForm] string? lines)
+	{
+		return CsvProcessor.LinesToCsv(lines);
+	}
+}
---
+++ StringProcessor/CsvProcessor.cs
@@ -0,0 +1,19 @@
+﻿namespace StringProcessor;
+
+public static class CsvProcessor
+{
+	public static string LinesToCsv(string? stringToProcess, string? surroundWith = null, bool toLower = false, char valueSeperator = ',')
+	{
+		if (string.IsNullOrEmpty(stringToProcess))
+			return string.Empty;
+
+		var lines = stringToProcess
+			.Split('\n')
+			.Where(line => !string.IsNullOrWhiteSpace(line))
+			.Select(line => line.Trim())
+			.OrderBy(line => line)
+			.ToList();
+
+		return string.Join(',', lines);
+	}
+}
