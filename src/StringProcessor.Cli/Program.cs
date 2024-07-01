7299c8ec0c1aa9e4613bfef54616a52c148c9575
PetesBreenCoding <pete.breen@icloud.com>
2024-07-01 11:29:42 +0100

    WIP

--- src/StringProcessor.Cli/Program.cs
+++ src/StringProcessor.Cli/Program.cs
@@ -1,7 +1,8 @@
-﻿using StringProcessor;
+﻿using System.Runtime.InteropServices;
+using StringProcessor;
 
 const string dataPath = "C:\\Data\\StringProcessor";
 
 var fileContent = await File.ReadAllTextAsync(Path.Combine(dataPath, "Input.txt"));
-fileContent = CsvProcessor.LinesToCsv(fileContent);
+fileContent = CsvProcessor.LinesToCsv(fileContent, newLineAfterEachValue: false);
 await File.WriteAllTextAsync(Path.Combine(dataPath, "Output.txt"), fileContent);
---
+++ src/StringProcessor/CsvProcessor.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace StringProcessor
+{
+	public static class CsvProcessor
+	{
+		public static string LinesToCsv(string stringToProcess, char valueSeparator = ',',
+			string surroundEachValueWith = null, bool toLower = false, bool newLineAfterEachValue = false,
+			bool trimEachValue = true)
+		{
+			if (string.IsNullOrEmpty(stringToProcess))
+				return string.Empty;
+
+			var lines = stringToProcess
+				.Split('\n')
+				.Where(line => !string.IsNullOrWhiteSpace(line))
+				.OrderBy(line => line)
+				.ToList();
+
+			var outputString = new StringBuilder();
+
+			for (var index = 0; index < lines.Count; index++)
+			{
+				var outputValue = lines[index];
+
+				if (trimEachValue)
+					outputValue = outputValue.Trim();
+
+				if (toLower)
+					outputValue = outputValue.ToLower();
+
+				if (!string.IsNullOrEmpty(surroundEachValueWith))
+					outputValue = $"{surroundEachValueWith}{outputValue}{surroundEachValueWith}";
+
+				if (index < lines.Count - 1)
+					outputValue += valueSeparator;
+
+				if (newLineAfterEachValue)
+					outputString.AppendLine(outputValue);
+				else
+					outputString.Append(outputValue);
+			}
+
+			return outputString.ToString();
+		}
+	}
+}
